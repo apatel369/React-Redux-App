3fe95b87b8a1b988e3464d313dfacd5d4707151a
Arpan Patel <ap.patel1893@gmail.com>
2019-04-27 23:50:39 -0500

    format store

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,48 +1,50 @@
-import * as actionTypes from './actions/actionTypes';
+import * as actionTypes from "./actions/actionTypes";
 
 const initialState = {
-    counter: 0,
-    results: []
-}
+  counter: 0,
+  results: []
+};
 
 const reducer = (state = initialState, action) => {
-    switch (action.type){
-        case actionTypes.INCREMENT:
-            return {
-                ...state,
-                counter: state.counter + 1
-            }
-        case actionTypes.DECREMENT:
-            return {
-                ...state,
-                counter: state.counter - 1
-            }
-        case actionTypes.ADD:
-            return {
-                ...state,
-                counter: state.counter + action.val
-            }
-        case actionTypes.SUBSTRACT:
-            return {
-                ...state,
-                counter: state.counter - action.val
-            }
-        case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({value: state.counter, id: new Date()})
-            }
-        case actionTypes.DELETE_RESULT:
-            // const id = 2;
-            // const newArray = [...state.results];
-            // newArray.splice(id, 1)
-            const updatedArray = state.results.filter(result=> result.id !== action.resultId);
-            return {
-                ...state,
-                results: updatedArray
-            }
-    }
-    return state;
-}
+  switch (action.type) {
+    case actionTypes.INCREMENT:
+      return {
+        ...state,
+        counter: state.counter + 1
+      };
+    case actionTypes.DECREMENT:
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+    case actionTypes.ADD:
+      return {
+        ...state,
+        counter: state.counter + action.val
+      };
+    case actionTypes.SUBSTRACT:
+      return {
+        ...state,
+        counter: state.counter - action.val
+      };
+    case actionTypes.STORE_RESULT:
+      return {
+        ...state,
+        results: state.results.concat({ value: state.counter, id: new Date() })
+      };
+    case actionTypes.DELETE_RESULT:
+      // const id = 2;
+      // const newArray = [...state.results];
+      // newArray.splice(id, 1)
+      const updatedArray = state.results.filter(
+        result => result.id !== action.resultId
+      );
+      return {
+        ...state,
+        results: updatedArray
+      };
+  }
+  return state;
+};
 
 export default reducer;
